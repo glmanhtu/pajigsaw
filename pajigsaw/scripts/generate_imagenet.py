2ec4f16e5406fb1007be0f8b7ebb6c796cb381cd
VU Manh Tu <glmanhtu@gmail.com>
2023-05-22 16:34:31 +0200

    Add script to generate imagenet patch db

--- pajigsaw/scripts/generate_imagenet.py
+++ pajigsaw/scripts/generate_imagenet.py
@@ -1,15 +1,12 @@
 import argparse
 import glob
 import os
-import random
 
 import torchvision.transforms
 import tqdm
 from PIL import Image, ImageOps
 
-from pajigsaw.data.datasets.papy_jigsaw import PapyJigSaw
 from pajigsaw.data.fragment import fragment_image
-from pajigsaw.utils.utils import split_list_by_ratios
 
 
 def get_args_parser(add_help: bool = True):
@@ -28,26 +25,23 @@ def get_args_parser(add_help: bool = True):
 
 if __name__ == '__main__':
     args = get_args_parser().parse_args()
-    image_paths = sorted(glob.glob(os.path.join(args.dataset_dir, '**', '*.png'), recursive=True))
-    random.seed(90)
-    random.shuffle(image_paths)
-    ratios = [split.length for split in PapyJigSaw.Split]
-    image_paths_splits = split_list_by_ratios(image_paths, ratios)
-
-    for split, image_paths in zip(PapyJigSaw.Split, image_paths_splits):
+    splits = ['val_images', 'train_images', 'test_images']
+    for split in splits:
         print(f'Processing split {split}')
+        image_paths = glob.glob(os.path.join(args.dataset_dir, split, '**', '*.JPEG'), recursive=True)
+        split_short = split.replace('_images', '')
         for image_path in tqdm.tqdm(image_paths):
             with Image.open(image_path) as f:
                 img = f.convert('RGB')
+                ratio = 2 * args.patch_size / min(img.width, img.height)
+                img = img.resize((int(ratio * img.width), int(ratio * img.height)), Image.LANCZOS)
             n_rows = max(round(img.height / args.patch_size), 1)
             n_cols = max(round(img.width / args.patch_size), 1)
             patches, im_cut = fragment_image(img, n_cols, n_rows)
             file_name = os.path.splitext(os.path.basename(image_path))[0]
-            if len(patches) < 2:
+            if len(patches) < 3:
                 continue
-            os.makedirs(os.path.join(args.output_dir, split.value, file_name), exist_ok=True)
-            os.makedirs(os.path.join(args.output_dir, 'cuts'), exist_ok=True)
-            im_cut.save(os.path.join(args.output_dir, 'cuts', f'{file_name}.jpg'))
+            os.makedirs(os.path.join(args.output_dir, split_short, file_name), exist_ok=True)
             for item in patches:
                 patch = item['img']
                 shape_trans = torchvision.transforms.Compose([
@@ -60,4 +54,4 @@ if __name__ == '__main__':
                 patch = patch.crop(bbox)
                 patch = ImageOps.invert(patch)
                 patch_name = f'{item["col"]}_{item["row"]}.png'
-                patch.save(os.path.join(args.output_dir, split.value, file_name, patch_name))
+                patch.save(os.path.join(args.output_dir, split_short, file_name, patch_name))

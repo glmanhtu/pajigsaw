dfd434d00136b37753d71ce759116d731bad0662
VU Manh Tu <glmanhtu@gmail.com>
2023-09-11 11:26:03 +0200

    Update readme minor changes

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ The training code is adapted from https://github.com/microsoft/Swin-Transformer
 ## Dataset generation
 
 ## Training
+
+## Evaluation
+Command to evaluation:
+>```CUDA_VISIBLE_DEVICES=0 python3.8 -m torch.distributed.run --standalone --nproc_per_node 1 evaluation.py --cfg configs/pajigsaw/eval_erosion7_4bin_patch8_64.yaml --data-path /data1/mvu/datasets/Puzzle --batch-size 128 --pretrained output/div2k_erosion7_4bin_patch8_64/default/best_model.pth --opts DATA.EROSION_RATIO 0.07```
--- visualise_dataset.py
+++ visualise_dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 import torchvision.transforms
 
-from data.datasets.imnet_patch import ImNetPatch
+from data.datasets.div2k_patch import DIV2KPatch
 from data.transforms import TwoImgSyncEval, UnNormalize
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
@@ -16,7 +16,7 @@ parser.add_argument('--data-path', required=True, type=str, help='path to datase
 args = parser.parse_args()
 
 transform = TwoImgSyncEval(224)
-train_dataset = ImNetPatch(args.data_path, split=ImNetPatch.Split.VAL, transform=transform)
+train_dataset = DIV2KPatch(args.data_path, split=DIV2KPatch.Split.TRAIN, transform=transform, with_negative=True)
 un_normaliser = torchvision.transforms.Compose([
     UnNormalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     torchvision.transforms.ToPILImage(),
@@ -25,12 +25,23 @@ un_normaliser = torchvision.transforms.Compose([
 for pair, label in train_dataset:
     first_img, second_img = torch.unbind(pair, dim=0)
     first_img, second_img = un_normaliser(first_img), un_normaliser(second_img)
-    image = np.concatenate([first_img, second_img], axis=0)
+    if label[0] == 1:
+        image = np.concatenate([first_img, second_img], axis=1)
+    elif label[2] == 1:
+        image = np.concatenate([second_img, first_img], axis=1)
+    elif label[3] == 1:
+        image = np.concatenate([second_img, first_img], axis=0)
+    elif label[1] == 1:
+        image = np.concatenate([first_img, second_img], axis=0)
+
+    else:
+        image = np.concatenate([first_img, np.zeros_like(first_img), second_img], axis=0)
+
     # image = cv2.bitwise_not(image)
     cv2.imshow('image', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
     # waitKey() waits for a key press to close the window and 0 specifies indefinite loop
-    cv2.waitKey(1000)
+    cv2.waitKey(5000)
 
     # cv2.destroyAllWindows() simply destroys all the windows we created.
 cv2.destroyAllWindows()

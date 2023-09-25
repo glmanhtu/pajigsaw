49a05ec3417c1fa5024c83e7597992bafbd82d7b
VU Manh Tu <glmanhtu@gmail.com>
2023-09-25 16:37:06 +0200

    Update

--- data/transforms.py
+++ data/transforms.py
@@ -112,6 +112,16 @@ def split_with_gap(im: Image, long_direction_ratio, gap: float):
     return patches
 
 
+def compute_white_percentage(img, ref_size=224):
+    gray = img.convert('L')
+    if gray.width > ref_size:
+        gray = gray.resize((ref_size, ref_size))
+    gray = np.asarray(gray)
+    white_pixel_count = np.sum(gray > 250)
+    total_pixels = gray.shape[0] * gray.shape[1]
+    return white_pixel_count / total_pixels
+
+
 class CustomRandomCrop:
     def __init__(self, crop_size, white_percentage_limit=0.6, max_retry=1000, im_path=''):
         self.cropper = torchvision.transforms.RandomCrop(crop_size, pad_if_needed=True, fill=255)
@@ -123,9 +133,7 @@ class CustomRandomCrop:
         current_retry = 0
         while current_retry < self.max_retry:
             out = self.cropper(img)
-            gray = np.array(out.convert('L'))
-            patch_bg_per = np.sum(gray == 255) / (gray.shape[0] * gray.shape[1])
-            if patch_bg_per <= self.white_percentage_limit:
+            if compute_white_percentage(out) <= self.white_percentage_limit:
                 return out
             current_retry += 1
         raise UnableToCrop('Unable to crop ', im_path=self.im_path)

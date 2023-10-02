c78e35c8521aaf6558bf2a91ad03c9fc0398cd39
VU Manh Tu <glmanhtu@gmail.com>
2023-10-02 17:39:41 +0200

    Update

--- hisfrag_test.py
+++ hisfrag_test.py
@@ -94,7 +94,7 @@ def testing(config, model):
         drop_last=False
     )
 
-    predicts = torch.zeros((0, 1), dtype=torch.float16)
+    predicts = torch.zeros((0, 1), dtype=torch.float16).cuda()
     indexes = torch.zeros((0, 2), dtype=torch.int32)
     batch_time = AverageMeter()
     start = time.time()
@@ -133,7 +133,7 @@ def testing(config, model):
 
             with torch.cuda.amp.autocast(enabled=config.AMP_ENABLE):
                 output = model(x2_images, x1_features)
-            predicts = torch.cat([predicts, output.cpu()])
+            predicts = torch.cat([predicts, output()])
             indexes = torch.cat([indexes, torch.column_stack([x1_id.expand(x2_indexes.shape[0]), x2_indexes + x1_id])])
             batch_time.update(time.time() - end)
             end = time.time()

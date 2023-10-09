e452176cc878e3d18757e7cd379204b5d8d5fb45
VU Manh Tu <glmanhtu@gmail.com>
2023-10-09 21:47:49 +0200

    Update

--- scripts/hisfrag_performance.py
+++ scripts/hisfrag_performance.py
@@ -6,14 +6,30 @@ import pandas as pd
 from misc import wi19_evaluate
 
 parser = argparse.ArgumentParser('Hisfrag20 testing script', add_help=False)
-parser.add_argument('--path', type=str, required=True)
+parser.add_argument('--pair-maps', type=str, required=True, nargs='+')
 args = parser.parse_args()
 
-with open(args.path, 'rb') as f:
-    similarity_map = pickle.load(f)
+similarity_map = {}
+
+for file_path in args.pair_maps:
+    with open(file_path, 'rb') as f:
+        pair_map = pickle.load(f)
+    for first_img in pair_map:
+        if first_img not in similarity_map:
+            similarity_map[first_img] = {}
+        for second_img in pair_map[first_img]:
+            if second_img not in similarity_map[first_img]:
+                similarity_map[first_img][second_img] = pair_map[first_img][second_img]
+            else:
+                similarity_map[first_img][second_img] += pair_map[first_img][second_img]
+                similarity_map[first_img][second_img] /= 2.
 
 matrix = pd.DataFrame.from_dict(similarity_map, orient='index').sort_index()
 matrix = matrix.reindex(sorted(matrix.columns), axis=1)
+
+total_cells = matrix.size
+cells_with_data = matrix.notnull().sum().sum()
+print('Total cells missing data:', total_cells - cells_with_data)
 
 m_ap, top1, pr_a_k10, pr_a_k100 = wi19_evaluate.get_metrics(matrix, lambda x: x.split("_")[0])
 

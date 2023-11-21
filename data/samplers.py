a54be7a5b5f72c23915dac6ea836a1e9331f44ae
Manh Tu VU <glmanhtu@gmail.com>
2023-11-21 13:01:56 +0100

    Optimise code

--- data/samplers.py
+++ data/samplers.py
@@ -22,30 +22,33 @@ class DistributedRepeatSampler(DistributedSampler):
         self.repeat = repeat
 
     def __iter__(self):
-        if self.shuffle:
-            # deterministically shuffle based on epoch and seed
-            g = torch.Generator()
-            g.manual_seed(self.seed + self.epoch)
-            indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore[arg-type]
-        else:
-            indices = list(range(len(self.dataset)))  # type: ignore[arg-type]
-
-        if not self.drop_last:
-            # add extra samples to make it evenly divisible
-            padding_size = self.total_size - len(indices)
-            if padding_size <= len(indices):
-                indices += indices[:padding_size]
+        all_indicates = []
+        for i in range(self.repeat):
+            if self.shuffle:
+                # deterministically shuffle based on epoch and seed
+                g = torch.Generator()
+                g.manual_seed(self.seed + self.epoch)
+                indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore[arg-type]
             else:
-                indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]
-        else:
-            # remove tail of data to make it evenly divisible.
-            indices = indices[:self.total_size]
-        assert len(indices) == self.total_size
+                indices = list(range(len(self.dataset)))  # type: ignore[arg-type]
 
-        # subsample
-        indices = indices[self.rank:self.total_size:self.num_replicas]
-        assert len(indices) == self.num_samples
-        return iter(indices * self.repeat)
+            if not self.drop_last:
+                # add extra samples to make it evenly divisible
+                padding_size = self.total_size - len(indices)
+                if padding_size <= len(indices):
+                    indices += indices[:padding_size]
+                else:
+                    indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]
+            else:
+                # remove tail of data to make it evenly divisible.
+                indices = indices[:self.total_size]
+            assert len(indices) == self.total_size
+
+            # subsample
+            indices = indices[self.rank:self.total_size:self.num_replicas]
+            assert len(indices) == self.num_samples
+            all_indicates += indices
+        return iter(all_indicates)
 
     def __len__(self) -> int:
         return self.num_samples * self.repeat
@@ -216,23 +219,26 @@ class DistributedEvalSampler(Sampler):
         self.seed = seed
 
     def __iter__(self):
-        if self.shuffle:
-            # deterministically shuffle based on epoch and seed
-            g = torch.Generator()
-            g.manual_seed(self.seed + self.epoch)
-            indices = torch.randperm(len(self.dataset), generator=g).tolist()
-        else:
-            indices = list(range(len(self.dataset)))
+        all_indices = []
+        for i in range(self.repeat):
+            if self.shuffle:
+                # deterministically shuffle based on epoch and seed
+                g = torch.Generator()
+                g.manual_seed(self.seed + self.epoch)
+                indices = torch.randperm(len(self.dataset), generator=g).tolist()
+            else:
+                indices = list(range(len(self.dataset)))
 
 
-        # # add extra samples to make it evenly divisible
-        # indices += indices[:(self.total_size - len(indices))]
-        # assert len(indices) == self.total_size
+            # # add extra samples to make it evenly divisible
+            # indices += indices[:(self.total_size - len(indices))]
+            # assert len(indices) == self.total_size
 
-        # subsample
-        indices = indices[self.rank:self.total_size:self.num_replicas]
-        assert len(indices) == self.num_samples
-        return iter(indices * self.repeat)
+            # subsample
+            indices = indices[self.rank:self.total_size:self.num_replicas]
+            assert len(indices) == self.num_samples
+            all_indices += indices
+        return iter(all_indices)
 
     def __len__(self):
         return self.num_samples * self.repeat

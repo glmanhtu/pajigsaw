2ead512e77f74ab9c61926d064adc44185f435f3
VU Manh Tu <glmanhtu@gmail.com>
2023-12-03 22:39:10 +0100

    Increase aug percentage

--- aem.py
+++ aem.py
@@ -255,7 +255,7 @@ class AEMTrainer(Trainer):
             torchvision.transforms.RandomApply([
                 torchvision.transforms.GaussianBlur((3, 3), (1.0, 2.0)),
                 torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
-            ], p=0.5),
+            ], p=0.7),
             torchvision.transforms.RandomCrop(img_size, pad_if_needed=True, fill=255),
             torchvision.transforms.RandomGrayscale(p=0.3),
             torchvision.transforms.ToTensor(),

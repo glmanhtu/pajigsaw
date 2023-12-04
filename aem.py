03938cd586ffba50648995ce8f629f8a5d5ec033
VU Manh Tu <glmanhtu@gmail.com>
2023-12-04 10:49:17 +0100

    Update

--- aem.py
+++ aem.py
@@ -256,9 +256,9 @@ class AEMTrainer(Trainer):
             torchvision.transforms.RandomApply([
                 torchvision.transforms.GaussianBlur((3, 3), (1.0, 2.0)),
             ], p=0.5),
-            # torchvision.transforms.RandomApply([
-            #     torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
-            # ], p=0.5),
+            torchvision.transforms.RandomApply([
+                torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
+            ], p=0.7),
             torchvision.transforms.RandomAffine(5, translate=(0.1, 0.1)),
             torchvision.transforms.RandomCrop(img_size, pad_if_needed=True, fill=255),
             torchvision.transforms.RandomGrayscale(p=0.3),

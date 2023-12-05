8d725c646d565c3964a755fbeda46dbd34053825
VU Manh Tu <glmanhtu@gmail.com>
2023-12-05 10:40:19 +0100

    Fix RandomResize fn

--- data/transforms.py
+++ data/transforms.py
@@ -127,7 +127,8 @@ class RandomResize:
 
     def __call__(self, img):
         ratio = random.uniform(self.ratio[0], self.ratio[1])
-        cropper = torchvision.transforms.Resize(int(ratio * self.img_size))
+        w, h = int(img.width * ratio), (img.height * ratio)
+        cropper = torchvision.transforms.Resize((h, w))
         return cropper(img)
 
 

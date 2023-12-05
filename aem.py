8de3e976bed5599c1ba85b3c52f1329152530831
VU Manh Tu <glmanhtu@gmail.com>
2023-12-05 11:01:12 +0100

    Update aug

--- aem.py
+++ aem.py
@@ -252,13 +252,13 @@ class AEMTrainer(Trainer):
                 A.LongestMaxSize(max_size=img_size),
                 A.ShiftScaleRotate(shift_limit=0, scale_limit=0.1, rotate_limit=15, p=0.5),
             ]),
-            # RandomResize(img_size, ratio=(0.8, 1.0)),
+            RandomResize(img_size, ratio=(0.8, 1.0)),
             torchvision.transforms.RandomApply([
                 torchvision.transforms.GaussianBlur((3, 3), (1.0, 2.0)),
             ], p=0.5),
             torchvision.transforms.RandomApply([
                 torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
-            ], p=0.7),
+            ], p=0.5),
             torchvision.transforms.RandomAffine(5, translate=(0.1, 0.1)),
             torchvision.transforms.RandomCrop(img_size, pad_if_needed=True, fill=255),
             torchvision.transforms.RandomGrayscale(p=0.3),

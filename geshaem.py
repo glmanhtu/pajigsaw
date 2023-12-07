ca33ab73d568539743b7cc5fc970855d59592df0
Manh Tu VU <glmanhtu@gmail.com>
2023-12-07 10:59:41 +0100

    Add triplet loss on geshaem training script

--- geshaem.py
+++ geshaem.py
@@ -50,6 +50,61 @@ def parse_option():
     return parser.parse_known_args()
 
 
+class TripletLoss(torch.nn.Module):
+    def __init__(self, margin=0.1):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+    def forward(self, emb, target):
+        return self.forward_impl(emb, target, emb, target)
+
+    def forward_impl(self, inputs_col, targets_col, inputs_row, targets_row):
+        n = inputs_col.size(0)
+        # Compute similarity matrix
+        sim_mat = torch.matmul(inputs_col, inputs_row.t())
+        # split the positive and negative pairs
+        eyes_ = torch.eye(n, dtype=torch.bool).cuda()
+        pos_mask = targets_col.expand(
+            targets_row.shape[0], n
+        ).t() == targets_row.expand(n, targets_row.shape[0])
+        neg_mask = ~pos_mask
+        pos_mask[:, :n] = pos_mask[:, :n] * ~eyes_
+
+        loss = list()
+        neg_count = list()
+        for i in range(n):
+            pos_pair_idx = torch.nonzero(pos_mask[i, :]).view(-1)
+            if pos_pair_idx.shape[0] > 0:
+                pos_pair_ = sim_mat[i, pos_pair_idx]
+                pos_pair_ = torch.sort(pos_pair_)[0]
+
+                neg_pair_idx = torch.nonzero(neg_mask[i, :]).view(-1)
+                neg_pair_ = sim_mat[i, neg_pair_idx]
+                neg_pair_ = torch.sort(neg_pair_)[0]
+
+                select_pos_pair_idx = torch.nonzero(
+                    pos_pair_ < neg_pair_[-1] + self.margin
+                ).view(-1)
+                pos_pair = pos_pair_[select_pos_pair_idx]
+
+                select_neg_pair_idx = torch.nonzero(
+                    neg_pair_ > max(0.6, pos_pair_[-1]) - self.margin
+                ).view(-1)
+                neg_pair = neg_pair_[select_neg_pair_idx]
+
+                pos_loss = torch.sum(1 - pos_pair)
+                if len(neg_pair) >= 1:
+                    neg_loss = torch.sum(neg_pair)
+                    neg_count.append(len(neg_pair))
+                else:
+                    neg_loss = 0
+                loss.append(pos_loss + neg_loss)
+            else:
+                loss.append(0)
+
+        loss = sum(loss) / n
+        return loss
+
+
 class SimSiamLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -86,7 +141,10 @@ class SimSiamLoss(torch.nn.Module):
 class GeshaemTrainer(Trainer):
 
     def get_criterion(self):
-        return SimSiamLoss()
+        if self.is_simsiam():
+            return SimSiamLoss()
+        else:
+            return TripletLoss(margin=0.15)
 
     def get_transforms(self):
         img_size = self.config.DATA.IMG_SIZE
@@ -141,6 +199,9 @@ class GeshaemTrainer(Trainer):
         self.data_loader_registers[mode] = data_loader
         return data_loader
 
+    def is_simsiam(self):
+        return 'ss' in self.config.MODEL.TYPE
+
     def validate_dataloader(self, data_loader):
         batch_time = AverageMeter()
         mAP_meter = AverageMeter()
@@ -156,7 +217,10 @@ class GeshaemTrainer(Trainer):
 
             # compute output
             with torch.cuda.amp.autocast(enabled=self.config.AMP_ENABLE):
-                p, z = self.model(images)
+                if self.is_simsiam():
+                    p, _ = self.model(images)
+                else:
+                    p = self.model(images)
 
             for feature, target in zip(p, targets.numpy()):
                 features.setdefault(target, [])

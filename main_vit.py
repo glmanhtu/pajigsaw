9dc722142648b12f026f8e1b4b0350f44ffd2b4e
VU Manh Tu <glmanhtu@gmail.com>
2024-01-16 07:28:08 +0100

    Update

--- main_vit.py
+++ main_vit.py
@@ -232,8 +232,6 @@ if __name__ == '__main__':
         trainer.validate()
     elif args.mode == 'throughput':
         trainer.throughput()
-    elif args.mode == 'test':
-        trainer.testing()
     else:
         trainer.train()
     trainer.testing()

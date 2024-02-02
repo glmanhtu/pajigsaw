8e89001117b55971e73a328251c55d8d85e0c83a
VU Manh Tu <glmanhtu@gmail.com>
2024-02-02 13:13:13 +0100

    Update

--- michigan.py
+++ michigan.py
@@ -147,7 +147,7 @@ class HisfragTrainer(Trainer):
                 else:
                     neg_groups = torch.cat((neg_groups, combinations), dim=0)
 
-        neg_length = min(neg_groups.shape[0], int(2 * pos_groups.shape[0]))
+        neg_length = min(neg_groups.shape[0], pos_groups.shape[0])
         neg_groups = neg_groups[torch.randperm(neg_groups.shape[0])[:neg_length]]
 
         labels = [1.] * pos_groups.shape[0] + [0.] * neg_groups.shape[0]
